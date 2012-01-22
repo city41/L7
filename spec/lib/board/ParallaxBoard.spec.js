7fe2fcaf47ea71e2d3e7e4fce6fac6d8be61d8be
Matt Greer <matt.e.greer@gmail.com>
2012-01-22 10:19:43 -0700

    added tests for ParallaxBoard

---
+++ spec/lib/board/ParallaxBoard.spec.js
@@ -0,0 +1,96 @@
+describe('ParallaxBoard', function() {
+	describe('construction', function() {
+		it('should default to an empty boards array', function() {
+			var p = new L7.ParallaxBoard();
+			expect(p.boards.length).toBe(0);
+		});
+
+		it('should throw if given a board without a parallax ratio', function() {	
+			var fn = function() {
+				new L7.ParallaxBoard({
+					boards: [{}]
+				});
+			};
+
+			expect(fn).toThrow();
+		});
+	});
+
+	describe('update', function() {
+		it('should have its children boards update', function() {
+			function createBoard() {
+				var b = {
+					parallaxRatio: 1,
+					update: function() {}
+				};
+				spyOn(b, 'update');
+				return b;
+			};
+
+			var board1 = createBoard();
+			var board2 = createBoard();
+
+			var p = new L7.ParallaxBoard({
+				boards: [board1, board2]
+			});
+
+			p.update();
+
+			expect(board1.update).toHaveBeenCalled();
+			expect(board1.update).toHaveBeenCalled();
+		});
+	});
+
+	describe('render', function() {
+		it('should have its children boards render, in order', function() {
+			var callCount = 0;
+			function createBoard() {
+				var b = {
+					parallaxRatio: 1,
+					render: function() {
+						this.callOrder = ++callCount;
+					}
+				};
+				return b;
+			};
+
+			var board1 = createBoard();
+			var board2 = createBoard();
+
+			var p = new L7.ParallaxBoard({
+				boards: [board1, board2]
+			});
+
+			p.render();
+
+			expect(board1.callOrder).toEqual(1);
+			expect(board2.callOrder).toEqual(2);
+		});
+
+		it('should use the boards parallaxRatio', function() {
+			var board = {
+				parallaxRatio: 0.45,
+				render: function() {
+
+				}
+			};
+
+			spyOn(board, 'render');
+
+			var p = new L7.ParallaxBoard({
+				boards: [board]
+			});
+
+			var delta = 0;
+			var context = {};
+			var anchorX = 10;
+			var anchorY = 20;
+			var timestamp = 1;
+
+			p.render(delta, context, anchorX, anchorY, timestamp);
+
+			expect(board.render).toHaveBeenCalledWith(delta, context, anchorX * board.parallaxRatio, anchorY * board.parallaxRatio, timestamp);
+		});
+	});
+
+});
