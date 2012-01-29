6753b386192f5a1679bd001808fae215426b1887
Matt Greer <matt.e.greer@gmail.com>
2012-01-29 06:30:35 -0700

    added Array.add and some Array specs

--- spec/lib/util/Array.spec.js
+++ spec/lib/util/Array.spec.js
@@ -1,5 +1,29 @@
 describe("Array", function() {
 	describe("added functions", function() {
+		describe("last", function() {
+			it("should return the last item", function() {
+				var a = [1, 2, 3, 4];
+
+				var last = a.last;
+
+				expect(last).toBe(4);
+			});
+			it("should return undefined if there is no last item", function() {
+				var a = [];
+
+				expect(a.last).not.toBeDefined();
+			});
+		});
+		describe("first", function() {
+			it("should return the first item", function() {
+				var a = [1,2,3];
+				expect(a.first).toBe(1);
+			});
+			it("should return undefined if there is no first item", function() {
+				var a = [];
+				expect(a.first).not.toBeDefined();
+			});
+		});
 		describe("remove", function() {
 			it("should remove an item", function() {
 
@@ -15,6 +39,21 @@ describe("Array", function() {
 			it("should return the array", function() {
 				var a = [1, 2, 3];
 				var ret = a.remove(4);
+
+				expect(ret).toBe(a);
+			});
+		});
+		describe("add", function() {
+			it("should add an item", function() {
+				var a = [1, 2, 4, 5];
+				a.add(7);
+
+				expect(a[a.length - 1]).toEqual(7);
+				expect(a.length).toEqual(5);
+			});
+			it("should return the array", function() {
+				var a = [1, 2, 3];
+				var ret = a.add(6);
 
 				expect(ret).toBe(a);
 			});
