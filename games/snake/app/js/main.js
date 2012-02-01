a3ed9b676759b25e5ec54cda131af213d6d4b34b
Matt Greer <matt.e.greer@gmail.com>
2012-02-01 06:20:06 -0700

    added Game.in, started on transition demo

--- games/snake/app/js/main.js
+++ games/snake/app/js/main.js
@@ -1,6 +1,17 @@
 (function() {
 		var _game;
 
+		function _onSplashComplete(splash) {
+			var nextBoard;
+			if(snake.Storage.present('hasPlayedBefore')) {
+				nextBoard = new snake.MainMenu
+			} else {
+				nextBoard = new snake.ClassicScenario
+			}
+
+			_game.transition(new L7.Fade(300), nextBoard);
+		}
+
 		snake.go = function(){
 			var splash = new snake.SplashScreen({
 				duration: 2000,
