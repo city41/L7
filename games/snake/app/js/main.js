4bd4c6e11c5729e7c5615682a2c872229df74276
Matt Greer <matt.e.greer@gmail.com>
2012-02-01 06:07:12 -0700

    getting overall scene structure in place

--- games/snake/app/js/main.js
+++ games/snake/app/js/main.js
@@ -1,6 +1,19 @@
 (function() {
+		var _game;
+
 		snake.go = function(){
-			
+			var splash = new snake.SplashScreen({
+				duration: 2000,
+				onComplete: _onSplashComplete;
+			});
+
+			_game = new L7.Game({
+				board: splash,
+				width: 480,
+				height: 320
+			});
+
+			_game.go();
 		};
 })();
 
