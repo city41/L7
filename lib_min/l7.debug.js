8fdd191ae444f6ccf08a1cdba76acef93bb5caae
Matt Greer <matt.e.greer@gmail.com>
2012-02-09 06:42:20 -0700

    deleted Sequence (just using Repeat set to 1 instead)

--- lib_min/l7.debug.js
+++ lib_min/l7.debug.js
@@ -50,7 +50,7 @@
 		},
 		_addAnimation: function(config, AniConstructor) {
 			if (!config.targets) {
-				if(config.filter) {
+				if(typeof config.filter !== 'undefined') {
 					config.targets = this._owner.getAnimationTargets(config.filter);
 				} else if(this._targetStack.length > 0) {
 					config.targets = this._targetStack.last;
@@ -549,44 +549,6 @@ Math.easeInOutBounce = function (t, b, c, d) {
 	};
 })();
 
-
-(function() {
-	L7.Sequence = function() {
-		this.children = [];
-		this._currentChild = 0;
-	};
-
-	L7.Sequence.prototype = {
-		reset: function() {
-			this.done = false;
-			this._currentChild = 0;
-
-			this.children.forEach(function(child) {
-				child.reset();
-			});
-		},
-
-		update: function() {
-			// TODO: problem, this is causing "time leak"
-			// children might have less than delta to go, if so, they will only
-			// go as far as they need to, and the rest of the delta gets thrown away
-			// if there is left over delta, it should go to the next child
-			this.done = this._currentChild >= this.children.length;
-
-			if (this.done) {
-				return;
-			}
-
-			var curChild = this.children[this._currentChild];
-
-			curChild.update.apply(curChild, arguments);
-
-			if (curChild.done) {++this._currentChild;
-			}
-			this.done = this._currentChild >= this.children.length;
-		}
-	};
-})();
 
 (function() {
 	var _idCounter = 0;
