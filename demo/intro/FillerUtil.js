3be127c22da4d9d178139d6ba06e219ddfa76788
Matt Greer <matt.e.greer@gmail.com>
2012-02-25 16:05:05 -0700

    added more animations

--- demo/intro/FillerUtil.js
+++ demo/intro/FillerUtil.js
@@ -1,13 +1,26 @@
 (function() {
+	var noColor = [121, 133, 164, 1];
 
 	i.FillerUtil = {
+		pulsate: function(board, position) {
+			board.ani.repeat(Infinity, function(ani) {
+				ani.shimmer({
+					targets: [board.tileAt(position)],
+					minAlpha: 0,
+					maxAlpha: 0.9,
+					baseRate: 600,
+					rateVariance: 0
+				});
+			});
+		},
+
 		addWater: function(board, corner, width, height) {
 			var bubbles = new L7.ParticleSystem({
 				totalParticles: 4,
 				duration: Infinity,
 				gravity: L7.p(),
 				centerOfGravity: L7.p(),
-				angle: -90,
+				angle: - 90,
 				angleVar: 0,
 				speed: 2,
 				speedVar: 0.4,
@@ -76,9 +89,8 @@
 			});
 		},
 
-		addBarGraph: function(board, position, width, height) {
-			var barColor = [240, 186, 89, 1];
-			var noColor = [100, 100, 100, 0.5];
+		addBarGraph: function(board, position, width, height, barColor) {
+			barColor = barColor || [240, 186, 89, 1];
 
 			var pieces = [];
 			for (var i = 0; i < height; ++i) {
@@ -100,7 +112,7 @@
 
 			function doBarGraph() {
 				for (var c = 0; c < width; ++c) {
-					var max = L7.rand(0, height + 1);
+					var max = L7.rand(0, height);
 					for (var r = 0; r < height; ++r) {
 						var piece = barGraph.pieces.filter(function(p) {
 							return p.position.x === c + position.x && p.position.y == r + position.y;
@@ -117,10 +129,60 @@
 
 			barGraph.ani.repeat(Infinity, function(ani) {
 				ani.invoke(doBarGraph);
-				ani.waitBetween(100, 2000);
+				ani.waitBetween(100, 1000);
+			});
+		},
+
+		addHeartWave: function(board, position, width, height, barColor) {
+			barColor = barColor || [255, 255, 0, 1];
+
+			var pieces = [];
+			for (var i = 0; i < height; ++i) {
+				var row = [];
+				for (var k = 0; k < width; ++k) {
+					row.push(1);
+				}
+				pieces.push(row);
+			}
+
+			pieces[0][0] = 5;
+
+			var heartWave = new L7.Actor({
+				shape: pieces,
+				position: position
+			});
+
+			board.addActor(heartWave);
+
+			var blipPosition = position.add(0, 1);
+			var nonBlipHeight = position.y + 2;
+
+			function doHeartWave() {
+				for (var c = 0; c < width; ++c) {
+					for (var r = 0; r < height; ++r) {
+						var piece = heartWave.pieces.filter(function(p) {
+							return p.position.x === c + position.x && p.position.y == r + position.y;
+						})[0];
+
+						if (piece.position.equals(blipPosition) || (piece.position.y === nonBlipHeight && piece.position.x !== blipPosition.x)) {
+							piece.color = barColor;
+						} else {
+							piece.color = noColor;
+						}
+					}
+				}
+
+				blipPosition = blipPosition.add(1, 0);
+				if (blipPosition.x >= position.x + width) {
+					blipPosition = position.add(0, 1);
+				}
+			}
+
+			heartWave.ani.repeat(Infinity, function(ani) {
+				ani.invoke(doHeartWave);
+				ani.wait(400);
 			});
 		}
 	};
 })();
-
 
--- demo/intro/MidForegroundFiller.js
+++ demo/intro/MidForegroundFiller.js
@@ -1,8 +1,17 @@
 i.MidForegroundFiller = {
 	fill: function(board) {
 		this._addControlPanelShimmer(board, board.rect(137, 11, 5, 3));
+		i.FillerUtil.addWater(board, L7.p(6, 5), 2, 6);
 		i.FillerUtil.addWater(board, L7.p(75, 1), 2, 5);
 		i.FillerUtil.addBlueScreen(board, L7.p(129, 11), 5, 3);
+		i.FillerUtil.addHeartWave(board, L7.p(30, 9), 5, 4);
+		i.FillerUtil.pulsate(board, L7.p(30, 14));
+		i.FillerUtil.pulsate(board, L7.p(32, 14));
+		i.FillerUtil.pulsate(board, L7.p(34, 14));
+		i.FillerUtil.addBarGraph(board, L7.p(46, 8), 4, 7, [0, 255, 0, 1]);
+		i.FillerUtil.pulsate(board, L7.p(79, 10));
+		i.FillerUtil.pulsate(board, L7.p(81, 10));
+		i.FillerUtil.pulsate(board, L7.p(83, 10));
 	},
 
 	_addControlPanelShimmer: function(board, targets) {
