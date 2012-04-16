fed2c833bda69b11796cb7a535b853d994f1c73b
Matt Greer <matt.e.greer@gmail.com>
2012-04-16 06:18:26 -0600

    got initial frame to show

---
+++ demo/frames/frames.js
@@ -0,0 +1,43 @@
+(function() {
+
+	function onImagesLoaded(images) {
+		var a = new L7.Actor({
+			framesConfig: {
+				src: images[0],
+				width: 9,
+				height: 14,
+				direction: 'horizontal',
+				sets: [
+					[0],
+					[1,2,3]
+				],
+				initialSet: 0,
+				initialFrame: 0,
+				anchor: L7.p(0,0)
+			},
+			position: L7.p(10, 10)
+		});
+
+		var board = new L7.Board({
+			width: 50,
+			height: 50,
+			tileSize: 8,
+			borderWidth: 0
+		});
+
+		board.addActor(a);
+
+		var game = new L7.Game(board);
+
+		game.fpsContainer = document.getElementById('fpsContainer');
+
+		game.go();
+	}
+
+	new L7.ImageLoader({
+		srcs: ['dance.png'],
+		handler: onImagesLoaded,
+		loadNow: true
+	});
+})();
+
